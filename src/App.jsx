b0fe6cb722811ef1bea051c5d87111cfd7781612
Himanshu <himanshubindal90340@gmail.com>
2025-02-10 20:45:30 +0530

    Added question card

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,53 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const currentQuestion=0;
+
+  const quizQuestions=[
+    {
+      question:"What is the complexity of the binary search",
+      options:[
+        { answer:'O(n)',isCorrect:false},
+        { answer:'O(log n)',isCorrect:true},
+        { answer:'O(1)',isCorrect:false},
+        { answer:'O(n^2)',isCorrect:false},
+      ]
+    },
+    {
+      question:"What is the complexity of the linear search",
+      options:[
+        { answer:'O(n)',isCorrect:true},
+        { answer:'O(log n)',isCorrect:false},
+        { answer:'O(1)',isCorrect:false},
+        { answer:'O(n^2)',isCorrect:false},
+      ]
+    },
+    {
+      question:"What is the complexity of the merge sort",
+      options:[
+        { answer:'O(n)',isCorrect:false},
+        { answer:'O(nlog n)',isCorrect:true},
+        { answer:'O(1)',isCorrect:false},
+        { answer:'O(n^2)',isCorrect:false},
+      ]
+    },      
+  ]
 
   return (
-    <>
-      <div>hello</div>
-    </>
+    <div className="app">
+        <div className="question-section">
+          <div className="question-count">
+            <span>Question {currentQuestion+1}</span>/ {quizQuestions.length}
+          </div>
+          <div className="question-text">
+            {quizQuestions[0].question}
+          </div>
+        </div>
+        <div className="answer-section">
+            {quizQuestions[0].options.map(option =><button>{option.answer}</button>)}
+        </div>
+    </div>
   )
 }
 
